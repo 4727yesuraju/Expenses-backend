c9c41cab3cf3474f19cbea9345be8c50387c5e16
yesuraju <4727yesuraju@gmail.com>
2024-07-19 10:42:57 +0530

    test removed

--- src/routes/users/Users.js
+++ src/routes/users/Users.js
@@ -3,7 +3,7 @@ const { test, userRegister, fetchUsers } = require("../../controllers/user/User"
 
 const userRouter = express.Router();
 
-userRouter.get("/",test);
+//userRouter.get("/",test);
 userRouter.post("/register",userRegister);
 userRouter.get("/",fetchUsers);
 
